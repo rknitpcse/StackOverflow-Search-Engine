7b8117f527a1e18cfc5c87029f95d141064628b8
rknitpcse <47469003+rknitpcse@users.noreply.github.com>
2020-09-05 15:53:50 +0530

    JSON Parsing using Jackson

--- stackoverflow-search-app/src/main/java/com/rakesh/adapter/StackOverFlowResult.java
+++ stackoverflow-search-app/src/main/java/com/rakesh/adapter/StackOverFlowResult.java
@@ -1,7 +1,11 @@
 package com.rakesh.adapter;
 
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 
@@ -13,34 +17,43 @@ import org.json.JSONObject;
 interface JSONInterface
 {
 	//A method with single parameter
-	public void jsonParse(JSONObject jsonObj);
+	public void jsonParse(MainParser mainParser);
 }
 
 
 public class StackOverFlowResult {
+	
+	private ObjectMapper objMapper = new ObjectMapper();
 
 	/**
 	 * Print/Display the top 5 search result
 	 * @param result
 	 * @param searchText
+	 * @throws Exception
 	 */
-	public void parseStringAsJSON(String result, String searchText) 
+	public void parseStringAsJSON(String result, String searchText) throws Exception 
 	{
+		
+		objMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+            false);
+
 		System.out.println("Here are the top-5 results for your search "+"'"+searchText+"':");
 		//parsing the result String into JSONObject
 		JSONObject jsonObj = new JSONObject(result);
 		JSONArray jsonArrayItems = (JSONArray) jsonObj.get("items");
 
 		//lambda function definition
-		JSONInterface jsonInterface = (jsonElement) -> System.out.print("\tTitle: "+ jsonElement.get("title") + ". URL: "+ jsonElement.get("link")+"\n");
+		JSONInterface jsonInterface = (parser) -> System.out.print("\tTitle: "+ parser.getTitle() + ". URL: "+ parser.getLink()+"\n");
 
 		//iteration of JSONArray
 		for(int i = 0; i < Math.min(jsonArrayItems.length(), 5); i++)
 		{
 			JSONObject jsonItr = (JSONObject) jsonArrayItems.get(i);
-			jsonInterface.jsonParse(jsonItr);
+			MainParser mainParser = objMapper.readValue(jsonItr.toString(), MainParser.class);
+			jsonInterface.jsonParse(mainParser);
 
 		}
+		
 
 	}
 
